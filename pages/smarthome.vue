8e4c4bf49f6c10a8783679c751e178411f402ed8
Marty McGee <support@companyjuice.com>
2022-09-11 12:44:07 -0700

    v0.7.8 Vue Garden on Nuxt + TypeScript + ThreeJS

---
+++ pages/smarthome.vue
@@ -0,0 +1,20 @@
+<template>
+  <SmartHome />
+</template>
+<script>
+import SmartHome from "@/components/dashboards/SmartHome.vue"
+
+definePageMeta({
+  // layout: "default",
+  // layout: "custom",
+  layout: "dashboard",
+  title: "Smart Home",
+})
+
+export default {
+  name: "Route: SmartHome",
+  components: {
+    SmartHome,
+  }
+}
+</script>
